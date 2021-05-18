4edc83fb194a9d5e4e4f8e9f0cdab69e241a017e
Leo5677 <leonardovalensoela567567@gmail.com>
2021-05-18 20:59:53 -0300

    Criando App Funcionarios

---
+++ funcionarios/admin.py
@@ -0,0 +1,10 @@
+from django.contrib import admin
+from .models import Funcionario
+
+
+# Register your models here.
+
+
+@admin.register(Funcionario)
+class FuncionarioAdmin(admin.ModelAdmin):
+    list_display = ['nome']
--- gestao_rh/settings.py
+++ gestao_rh/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'empresas',
+    'funcionarios',
 ]
 
 MIDDLEWARE = [
